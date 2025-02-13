9136598e6b55e901d4f1e831c115f99dab99677d
hello672fz <1294581453@qq.com>
2025-02-13 22:08:27 +0800

    scale criu mm-template size

--- criu/include/cr_options.h
+++ criu/include/cr_options.h
@@ -74,7 +74,9 @@ enum NETWORK_LOCK_METHOD {
 /*
  * Ghost file size we allow to carry by default.
  */
-#define DEFAULT_GHOST_LIMIT (1 << 20)
+// #define DEFAULT_GHOST_LIMIT (1 << 20)
+
+#define DEFAULT_GHOST_LIMIT (1 << 25)
 
 #define DEFAULT_TIMEOUT 10
 
